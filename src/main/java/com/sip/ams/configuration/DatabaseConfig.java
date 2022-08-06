edb0ceb2cdbff1270e23db3dbbe142c99a9e3310
zorganimed <zorganimed.mohamed@gmail.com>
2022-08-06 22:28:34 +0200

    version avec postgres

---
+++ src/main/java/com/sip/ams/configuration/DatabaseConfig.java
@@ -0,0 +1,23 @@
+package com.sip.ams.configuration;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.*;
+
+import com.zaxxer.hikari.*;
+
+@Configuration
+public class DatabaseConfig {
+
+	@Value("${spring.datasource.url}")
+	private String dbUrl;
+
+	public DataSource datSource() {
+
+		HikariConfig config = new HikariConfig();
+		config.setJdbcUrl(dbUrl);
+		return new HikariDataSource(config);
+	}
+
+}
